35586c77e174d583c6ceb1f5e6a6f74c050e3670
John Wood <watch_wood@hotmail.com>
2025-03-09 22:03:47 -0400

    Tweaks

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use App\Models\Item;
@@ -12,17 +10,13 @@ use App\Models\CartItem;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
-
 
     public function index(Request $request) {
 		
 		//using the hex session ID for a user ID since a user system is out of scope for the project
 		$userId = $request->session()->getId();
 
-		//$request->session()->put('initialized', true);
-		//create example data on first load
-		//dd($request->session()->get('initialized'));
+		//create example data on first load for a session
 		if ($request->session()->get('initialized') != true){
 			$request->session()->put('initialized', true);
 
@@ -33,11 +27,6 @@ class Controller extends BaseController
 				]);
 			}
 		}
-
-		//dd($userId);
-		//dd($request->session()->all());
-		//dd(CartItem::get());
-		//dd(CartItem::where("user_id", $userId)->get());
 
 		return view('home',  [
 			"items" => CartItem::with("item")->where("user_id", $userId)->get()->toArray(),
